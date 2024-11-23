fd3a4d12fab75e9905e2a8399dadd375d855758f
Waqar-Rajpoot <rajpoottony61@gmail.com>
2024-11-23 06:15:51 -0800

    finalizing project

--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -1,13 +1,11 @@
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asynHandler} from "../utils/asynHandler.js"
-
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asynHandler } from "../utils/asynHandler.js";
 
 const healthcheck = asynHandler(async (req, res) => {
-    //TODO: build a healthcheck response that simply returns the OK status as json with a message
-})
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Yeh! everything is okay!"));
+});
 
-export {
-    healthcheck
-    }
-    
+export { healthcheck };
